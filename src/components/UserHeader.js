7bd7b6ef4492fa4fd51e2ad23303765066f2a18e
Tyler <tylerhines1992@gmail.com>
2019-10-05 16:17:17 -0400

    refactor to only make 1 network request per userId

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-	componentDidMount() {
-		this.props.fetchUser(this.props.userId);
-	}
-
 	render() {
+		console.log(this.props);
 		const { user } = this.props;
 
 		if (!user) {
@@ -22,7 +18,4 @@ const mapStateToProps = (state, ownProps) => {
 	return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(
-	mapStateToProps,
-	{ fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
